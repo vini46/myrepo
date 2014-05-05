14b8dac2142ac9d323cd355060605a31d1eae4be
vinod baradwaj <vinod.baradwaj@live.com>
2014-05-05 11:59:42 +0530

    update

--- app.js
+++ app.js
@@ -6,8 +6,52 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/test1');
+
+var mongolian = require('mongolian');
+var server, db;
+
+if(process.env.MONGOHQ_URL) {
+  db = new Mongolian(process.env.MONGOHQ_URL);
+} else {
+  server = new Mongolian;
+  db = server.db('localhost:27017/test1');
+}
+
+/* mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db){
+  db.addListener("error", function(error){
+    console.log("error connecting to MongoLab");
+  });
+}); */
+
+/*var server = new Server('ds037358.mongolab.com:37358', 12345, {auto_reconnect : true});
+var db = new Db('heroku_app24851292', server);
+
+
+db.open(function(err, client) {
+    client.authenticate('vinod.baradwaj@live.com', 'helloworld@123', function(err, success) {
+        // Do Something ...
+    });
+}); */
+
+/*var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://heroku_app24851292:rka0p8tnmujh9s5aidapvni2e5@ds037358.mongolab.com:37358/heroku_app24851292';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+}); */
+
+//var monk = require('monk');
+//var db = monk('localhost:27017/test1');
+
+/*var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'ds037358.mongolab.com:37358/heroku_app24851292'; **/
+
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
